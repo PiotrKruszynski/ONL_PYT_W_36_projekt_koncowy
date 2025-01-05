400ca72065a988f941be3bee54d211cc5d62c4b8
Kruszon <piotr@Pios-MacBook-Pro.local>
2025-01-05 22:54:47 +0100

    db_sqlite

--- lab_manager/settings.py
+++ lab_manager/settings.py
@@ -86,6 +86,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'pk_db_01',
+#         'USER': 'PiotrKruszynski',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
